f4b636464a7c7f7fc6ab7ac58598799bbfee2602
LKDuongCode <duongleeaaa@gmail.com>
2024-06-07 11:25:24 +0700

    thêm thông tin bài làm

---
+++ SS55_homework/src/components/h1-h10/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function index() {
+  /*
+    h1. tạo mock api
+    h2. tạo dữ liệu bài viết
+    h3. lấy thông tin tất cả post trên postman
+    h4. lấy thông tin post trên id.
+    h5. xóa một post theo id
+    h6. lấy thông tin tất cả theo tác giả
+    h7. thêm mới post.
+    h8. cập nhật tất cả trường một post.
+    h9. cập nhật một trường của một post.
+    h10.cập nhật bình luận theo id.
+    */
+  return <div>index</div>;
+}
