a300c7053fd243e7e31e5c40020fdb9c87e28982
LKDuongCode <duongleeaaa@gmail.com>
2024-06-27 11:36:39 +0700

    big update

--- client/src/pages/Cart.tsx
+++ client/src/pages/Cart.tsx
@@ -5,10 +5,12 @@ import { fetchCarts } from "../service/cartService/getCart.service";
 
 export default function Cart() {
   // modal setting
-  let [check, setCheck] = useState<boolean>(false);
+  let [checkDel, setCheckDel] = useState<boolean>(false);
+  let [checkUp, setCheckUp] = useState<boolean>(false);
   let [alertAdd, setAlertAdd] = useState<boolean>(false);
   let [alertUpdate, setAlertUpdate] = useState<boolean>(false);
   let [alertDelete, setAlertDelete] = useState<boolean>(false);
+
   // modal setting
 
   //lấy dữ liệu carts
@@ -58,16 +60,18 @@ export default function Cart() {
 
                   <button
                     className="py-2 px-3 flex justify-center items-center"
-                    onClick={() => setAlertUpdate(true)}
+                    onClick={() => {
+                      setCheckUp(true);
+                    }}
                   >
                     <p>Update</p>
                   </button>
                   <button
                     className={`py-2 px-3 flex justify-center items-center ${
-                      check ? "z-[auto]" : "z-[1]"
+                      checkDel ? "z-[auto]" : "z-[1]"
                     }`}
                     onClick={() => {
-                      setCheck(true);
+                      setCheckDel(true);
                     }}
                   >
                     <p>Remove</p>
@@ -107,14 +111,14 @@ export default function Cart() {
       {/* modal delete confirm */}
       <div className="z-[0]">
         <div
-          className={`relative ${check ? "z-[1]" : "z-[-1]"}`}
+          className={`relative ${checkDel ? "z-[1]" : "z-[-1]"}`}
           aria-labelledby="modal-title"
           role="dialog"
           aria-modal="true"
         >
           <div
             className={`fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity ${
-              check
+              checkDel
                 ? "ease-out duration-300 opacity-100 "
                 : "ease-in duration-200 opacity-0"
             } `}
@@ -122,17 +126,17 @@ export default function Cart() {
 
           <div
             onClick={() => {
-              setCheck(false);
+              setCheckDel(false);
             }}
             className={`fixed inset-0 z-10 w-screen overflow-y-auto  ${
-              check
+              checkDel
                 ? "ease-out duration-300 opacity-100 "
                 : "ease-in duration-200 opacity-0 "
             }`}
           >
             <div
               className={`flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0 ${
-                check
+                checkDel
                   ? "ease-out duration-300  translate-y-0 sm:scale-100 "
                   : "ease-in duration-200  translate-y-4 sm:translate-y-0 sm:scale-95 "
               }`}
@@ -177,8 +181,9 @@ export default function Cart() {
                     type="button"
                     className=" border-none inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
                     onClick={() => {
-                      setCheck(false);
+                      setCheckDel(false);
                       setAlertDelete(true);
+                      setAlertUpdate(false);
                     }}
                   >
                     Delete
@@ -187,7 +192,102 @@ export default function Cart() {
                     type="button"
                     className=" border-none mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
                     onClick={() => {
-                      setCheck(false);
+                      setCheckDel(false);
+                    }}
+                  >
+                    Cancel
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* modal update confirm */}
+      <div className="z-[0]">
+        <div
+          className={`relative ${checkUp ? "z-[1]" : "z-[-1]"}`}
+          aria-labelledby="modal-title"
+          role="dialog"
+          aria-modal="true"
+        >
+          <div
+            className={`fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity ${
+              checkUp
+                ? "ease-out duration-300 opacity-100 "
+                : "ease-in duration-200 opacity-0"
+            } `}
+          ></div>
+
+          <div
+            onClick={() => {
+              setCheckUp(false);
+            }}
+            className={`fixed inset-0 z-10 w-screen overflow-y-auto  ${
+              checkUp
+                ? "ease-out duration-300 opacity-100 "
+                : "ease-in duration-200 opacity-0 "
+            }`}
+          >
+            <div
+              className={`flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0 ${
+                checkUp
+                  ? "ease-out duration-300  translate-y-0 sm:scale-100 "
+                  : "ease-in duration-200  translate-y-4 sm:translate-y-0 sm:scale-95 "
+              }`}
+            >
+              <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                  <div className="sm:flex sm:items-start">
+                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-yellow-100 sm:mx-0 sm:h-10 sm:w-10">
+                      <svg
+                        className="h-6 w-6 text-yellow-600"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        aria-hidden="true"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
+                        />
+                      </svg>
+                    </div>
+                    <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                      <h3
+                        className="text-base font-semibold leading-6 text-gray-900"
+                        id="modal-title"
+                      >
+                        Warning
+                      </h3>
+                      <div className="mt-2">
+                        <p className="text-sm text-gray-500">
+                          Are you sure you want to update? All of your data will
+                          be updated. This action cannot be undone.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                  <button
+                    type="button"
+                    className=" border-none inline-flex w-full justify-center rounded-md bg-yellow-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-yellow-500 sm:ml-3 sm:w-auto"
+                    onClick={() => {
+                      setCheckUp(false);
+                      setAlertDelete(false);
+                      setAlertUpdate(true);
+                    }}
+                  >
+                    Update
+                  </button>
+                  <button
+                    type="button"
+                    className=" border-none mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                    onClick={() => {
+                      setCheckUp(false);
                     }}
                   >
                     Cancel
--- client/src/pages/Sell.tsx
+++ client/src/pages/Sell.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import type { CombineType } from "../model/modelType";
 import type { Sell } from "../model/modelType";
 import { useDispatch, useSelector } from "react-redux";
@@ -6,6 +6,9 @@ import { fetchSells } from "../service/sellService/getSells.service";
 import { addToCarts } from "../service/cartService/addCart.service";
 // lưu ý : import type cần dùng import type để phân biệt với module.
 export default function Sell() {
+  // modal setting
+  let [checkAdd, setCheckAdd] = useState<boolean>(false);
+  // modal setting
   //lấy dữ liệu
   let sells: Sell[] = useSelector((state: CombineType) => {
     return state.sells.data;
@@ -32,44 +35,124 @@ export default function Sell() {
   };
 
   return (
-    <div className="border-2 border-solid p-5 w-2/3 bg-white">
-      <h3 className="text-center">Product List</h3>
+    <>
+      <div className="border-2 border-solid p-5 w-2/3 bg-white">
+        <h3 className="text-center">Product List</h3>
 
-      <ul className=" p-5 flex flex-col gap-4">
-        {sells.map((item: Sell, index: number) => {
-          return (
-            <li className="h-32  p-2 flex gap-2 bg-slate-100 " key={item.id}>
-              <div>
-                <img
-                  src={item.image}
-                  alt=""
-                  className="h-full w-28 border-2 border-solid"
-                />
+        <ul className=" p-5 flex flex-col gap-4">
+          {sells.map((item: Sell, index: number) => {
+            return (
+              <li className="h-32  p-2 flex gap-2 bg-slate-100 " key={item.id}>
+                <div>
+                  <img
+                    src={item.image}
+                    alt=""
+                    className="h-full w-28 border-2 border-solid"
+                  />
+                </div>
+                <div className="flex flex-col justify-between w-full">
+                  <p className="font-semibold">{item.name}</p>
+                  <p>{item.description}</p>
+                  <p className="font-semibold">Total : {item.quantity}</p>
+                </div>
+                <div className="flex flex-col justify-between w-20">
+                  <input
+                    type="number"
+                    className="py-2 text-center"
+                    readOnly
+                    value={1}
+                  />
+                  <p>Price:{item.price}$</p>
+                  <button
+                    className="py-2 flex justify-center items-center"
+                    onClick={() => {
+                      handleAdd(item.id);
+                      setCheckAdd(true);
+                    }}
+                  >
+                    <p>Add cart</p>
+                  </button>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      {/* modal add */}
+      <div className="z-[0]">
+        <div
+          className={`relative ${checkAdd ? "z-[1]" : "z-[-1]"}`}
+          aria-labelledby="modal-title"
+          role="dialog"
+          aria-modal="true"
+        >
+          <div
+            className={`fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity ${
+              checkAdd
+                ? "ease-out duration-300 opacity-100 "
+                : "ease-in duration-200 opacity-0"
+            } `}
+          ></div>
+
+          <div
+            onClick={() => {
+              setCheckAdd(false);
+            }}
+            className={`fixed inset-0 z-10 w-screen overflow-y-auto  ${
+              checkAdd
+                ? "ease-out duration-300 opacity-100 "
+                : "ease-in duration-200 opacity-0 "
+            }`}
+          >
+            <div
+              className={`flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0 ${
+                checkAdd
+                  ? "ease-out duration-300  translate-y-0 sm:scale-100 "
+                  : "ease-in duration-200  translate-y-4 sm:translate-y-0 sm:scale-95 "
+              }`}
+            >
+              <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                  <div className="sm:flex sm:items-start">
+                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-green-100 sm:mx-0 sm:h-10 sm:w-10">
+                      <svg
+                        className="h-6 w-6 text-green-600"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        aria-hidden="true"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M12 4v16m8-8H4"
+                        />
+                      </svg>
+                    </div>
+                    <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                      <h3
+                        className="text-base font-semibold leading-6 text-gray-900"
+                        id="modal-title"
+                      >
+                        Added to cart successfully
+                      </h3>
+                      <div className="mt-2">
+                        <p className="text-sm text-gray-500">
+                          This item was added to your carts successfully ! Now,
+                          you can check your carts and are continuing to shop.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6"></div>
               </div>
-              <div className="flex flex-col justify-between w-full">
-                <p className="font-semibold">{item.name}</p>
-                <p>{item.description}</p>
-                <p className="font-semibold">Total : {item.quantity}</p>
-              </div>
-              <div className="flex flex-col justify-between w-20">
-                <input
-                  type="number"
-                  className="py-2 text-center"
-                  readOnly
-                  value={1}
-                />
-                <p>Price:{item.price}$</p>
-                <button
-                  className="py-2 flex justify-center items-center"
-                  onClick={() => handleAdd(item.id)}
-                >
-                  <p>Add cart</p>
-                </button>
-              </div>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
